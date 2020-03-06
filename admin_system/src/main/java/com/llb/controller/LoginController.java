5aa45d4eb74e8bc627532ede5efc08d5af283985
Keep Learning <liulebinn@163.com>
2020-03-06 16:17:48 +0800

    login页面修改

--- admin_system/src/main/java/com/llb/controller/LoginController.java
+++ admin_system/src/main/java/com/llb/controller/LoginController.java
@@ -2,6 +2,10 @@ package com.llb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * 登录
@@ -15,6 +19,21 @@ public class LoginController {
     @RequestMapping("/index")
     public String toLogin() {
         return "index";
+    }
+
+    /**
+     * 登录 用户/教练/管理员 登录
+     * @param username 登录名
+     * @param password 密码
+     * @param type 类型:1.用户 2.管理员 3.教练
+     * @return
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Map<String, Object> login(@RequestParam(required = true) String username,
+                                     @RequestParam(required = true) String password,
+                                     @RequestParam(required = true) Integer type) {
+
+        return null;
     }
 
     @RequestMapping("/welcome")
