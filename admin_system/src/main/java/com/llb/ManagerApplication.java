2e30fca3ec211503d0f32a40792dc9ef829b62d1
Keep Learning <liulebinn@163.com>
2020-03-25 09:13:53 +0800

    部署配置修改

--- admin_system/src/main/java/com/llb/ManagerApplication.java
+++ admin_system/src/main/java/com/llb/ManagerApplication.java
@@ -3,6 +3,8 @@ package com.llb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 启动类
@@ -11,8 +13,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.llb.mapper")
-public class ManagerApplication {
+public class ManagerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ManagerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
     }
 }
